860dfa9f2ebee413e90c503198e237116b374231
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2024-02-11 00:34:31 +0100

    fail

--- MyMathTests/MyMathTests.swift
+++ MyMathTests/MyMathTests.swift
@@ -28,7 +28,7 @@ final class MyMathTests: XCTestCase {
         let expectedResult = 600
         XCTAssertEqual(expectedResult, rect.area())
         let randomNumber = MyMath.RandomNumber()
-        let expectedNumber: Int32 = 42
+        let expectedNumber: Int32 = 43
         XCTAssertEqual(expectedNumber, randomNumber.randomNumber())
     }
 
